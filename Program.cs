68a38221e01562917b2a71f97f3cb202945cad97
TrahsPanda <zupdike17@gmail.com>
2022-01-20 21:59:29 -0500

    1.1

--- Program.cs
+++ Program.cs
@@ -1,32 +1,55 @@
 ﻿using System;
+using System.Globalization;
 
 namespace NameGenerator
 {
     class NameGenerator
     {
-        static Random random = new Random();
-        static string[] con = new string[] { "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z" };
-        static string[] conCap = new string[] { "B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z" };
-        static string[] vow = new string[] { "a", "e", "i", "o", "u" };
-        static string[] vowCap = new string[] { "A", "E", "I", "O", "U" };
+        public static Random random = new Random();
+        public static TextInfo ti = CultureInfo.CurrentCulture.TextInfo;
 
-        static void Main()
+        public static string[] con = new string[] { "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z" };
+        public static string[] vow = new string[] { "a", "e", "i", "o", "u" };
+        public static int conLength = con.Length;
+        public static int vowLength = vow.Length;
+        public static void Main()
         {
             Console.WriteLine("Welcome to TrahsPanda's Random Name Generator!\nHow many names would you like to generate?");
-            int namesToGenerate = Int32.Parse(Console.ReadLine()); // Warning can be safetly ignored
+            int namesToGenerate = Convert.ToInt32(Console.ReadLine());
             for (int i = 0; i < namesToGenerate; i++)
             {
-                if (random.NextDouble() < 0.1) // Zachary format
+                int firstDecider = random.Next(11);
+                int secondDecider = random.Next(11);
+                if (firstDecider == 0) // CVCCVCC 
                 {
-                    int conCapIndex = random.Next(conCap.Length); //Z
-                    int vowIndex = random.Next(vow.Length); //a
-                    int conIndex = random.Next(con.Length); //c
-                    int conIndex1 = random.Next(con.Length); //h
-                    int vowIndex1 = random.Next(vow.Length); //a
-                    int conIndex2 = random.Next(con.Length); //r
-                    int conIndex3 = random.Next(con.Length); //y
-                    Console.WriteLine(conCap[conCapIndex] + vow[vowIndex] + con[conIndex] + con[conIndex1] + vow[vowIndex1] + con[conIndex2] + con[conIndex3]);
+                    int conIndex = random.Next(conLength); 
+                    int vowIndex = random.Next(vowLength); 
+                    int conIndex1 = random.Next(conLength); 
+                    int conIndex2 = random.Next(conLength); 
+                    int vowIndex1 = random.Next(vowLength); 
+                    int conIndex3 = random.Next(conLength); 
+                    int conIndex4 = random.Next(conLength); 
+                    Console.Write(ti.ToTitleCase(con[conIndex] + vow[vowIndex] + con[conIndex1] + con[conIndex2] + vow[vowIndex1] + con[conIndex3] + con[conIndex4]));
                 }
+                else if (firstDecider == 1) // CVCVC
+                {
+                    int conIndex = random.Next(conLength);
+                    int vowIndex = random.Next(vowLength);
+                    int conIndex1 = random.Next(conLength);
+                    int vowIndex1 = random.Next(vowLength);
+                    int conIndex2 = random.Next(conLength);
+                    int vowIndex2 = random.Next(vowLength);
+                    Console.Write(ti.ToTitleCase(con[conIndex] + vow[vowIndex] + con[conIndex1] + vow[vowIndex1] + con[conIndex2] + vow[vowIndex2]));
+                }
+                else if (firstDecider == 2) // CVCC
+                {
+                    int conIndex = random.Next(conLength);
+                    int vowIndex = random.Next(vowLength);
+                    int conIndex1 = random.Next(conLength);
+                    int conIndex2 = random.Next(conLength);
+                    Console.Write(ti.ToTitleCase(con[conIndex] + vow[vowIndex] + con[conIndex1] + con[conIndex2]));
+                }
+                
             }
         }
     }
